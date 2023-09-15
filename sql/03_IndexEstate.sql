3cf2d6ac09543b659cf026850881399cb3a3b8db
azbcwww <1121hatsuyuki@gmail.com>
2023-09-15 20:42:01 +0000

    add index to categories for id

--- sql/03_IndexEstate.sql
+++ sql/03_IndexEstate.sql
@@ -1,3 +1,4 @@
 USE isucari;
 ALTER TABLE `items` ADD INDEX `created_at_idx` (`created_at` DESC, `id` DESC);
 ALTER TABLE `items` ADD INDEX `seller_id_idx` (`seller_id`);
+ALTER TABLE `categories` ADD INDEX `id_idx` (`id`);
