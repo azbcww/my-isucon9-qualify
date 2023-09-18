67f415ae3125969ab652ff31602dbb0e60ce787b
azbcwww <1121hatsuyuki@gmail.com>
2023-09-18 06:05:47 +0000

    add index to items for buyer_id

--- sql/03_IndexEstate.sql
+++ sql/03_IndexEstate.sql
@@ -1,4 +1,5 @@
 USE isucari;
 ALTER TABLE `items` ADD INDEX `created_at_idx` (`created_at` DESC, `id` DESC);
 ALTER TABLE `items` ADD INDEX `seller_id_idx` (`seller_id`);
+ALTER TABLE `items` ADD INDEX `buyer_id_idx` (`buyer_id`);
 ALTER TABLE `categories` ADD INDEX `id_idx` (`id`);
