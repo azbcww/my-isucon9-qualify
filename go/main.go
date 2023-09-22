ec08760f04fa757d45802a51fbf67e3cec113e1d
azbcwww <1121hatsuyuki@gmail.com>
2023-09-22 20:45:55 +0000

    Revert "remove conflict"
    This reverts commit 5d85e6b36d7901c1c6adfd02d4c091229692acd5, reversing
    changes made to f6288fbc79458b2f64a4769e4ac36370acef13e3.

--- go/main.go
+++ go/main.go
@@ -1124,10 +1124,7 @@ func getTransactions(w http.ResponseWriter, r *http.Request) {
 		// }
 
 		err := tx.Select(&items,
-			"SELECT * FROM `items` WHERE `seller_id` = ? UNION ALL SELECT * FROM `items` WHERE `buyer_id` = ? ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
-			user.ID,
-			user.ID,
-			TransactionsPerPage+1,
+			"SELECT * FROM `items` WHERE `seller_id` = ? UNIO ALL SELECT * FROM `items` WHERE `buyer_id` = ? ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
 		)
 
 		if err != nil {
